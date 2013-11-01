e8090b37001c3701a7789d239c17b8781653a840
sreejesh <sreejeshkp@agileblaze.com>
2013-11-01 16:05:07 +0530

    rails admin added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MyBase::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   root :to => "home#index"
 
   devise_for :users, :controllers => {:registrations => "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks"}, 
